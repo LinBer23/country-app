717191c14940b0b484a3d598f17c68833cd19dfe
Ines <link_ines@gmx.de>
2024-02-19 10:55:41 +0100

    styling countriesItem, implemented searchByInput and searchByRegion

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import "./App.css";
 
 const App = () => {
     const [countries, setCountries] = useState([]);
-    const [input, setInput] = useState([]);
-    console.log(countries);
+    const [input, setInput] = useState("");
+    const [filterRegion, setFilterRegion] = useState("");
 
     const fetchCountries = async () => {
         const RESPONSE = await fetch("https://restcountries.com/v3.1/all");
@@ -22,6 +22,53 @@ const App = () => {
         fetchCountries();
     }, []);
 
+    /*  const x = 5;
+
+    function doNothing() {
+        return "aa";
+    } */
+
+    function currentContinent(countries) {
+        /*    const region = ["Africa", "Europe", "Asien"];
+        return region.map((continents, index) => (
+            <option key={index} value={continents}>
+                {continents}
+            </option>
+        )); */
+        const regions = countries.map((country) => country.region);
+        const uniqueRegion = Array.from(new Set(regions));
+
+        return uniqueRegion.map((region, index) => {
+            return (
+                <option key={index} value={region}>
+                    {region}
+                </option>
+            );
+        });
+    }
+    function handleSelectChange(e) {
+        setFilterRegion(e.target.value);
+    }
+    /*  function handleInputChange(e) {
+        setInput(e.target.value);
+    } */
+
+    /* function displayOption(currentOption) {
+        return <option value={currentOption}>{currentOption}</option>;
+    } */
+
+    const filteredByInput = countries.filter((country) => {
+        return country.name.common.toLowerCase().includes(input.toLowerCase());
+    });
+    console.log(filteredByInput);
+
+    const filteredCountriesByContinent = filteredByInput.filter((country) => {
+        if (filterRegion === "") {
+            return true;
+        }
+        return country.region === filterRegion;
+    });
+
     return (
         <>
             <header>
@@ -34,22 +81,30 @@ const App = () => {
                 </div>
             </header>
             <hr />
+            <filter-wrapper>
+                <input
+                    type="search"
+                    onChange={(e) => setInput(e.target.value)}
+                    className="inputField"
+                    placeholder="Search Country"
+                />
+
+                <select onChange={handleSelectChange}>
+                    <option value="">Select By Region</option>
+                    {currentContinent(countries)}
+                </select>
+            </filter-wrapper>
             <main className="wrapper">
-                <filter-wrapper>{}
-                    <input type="search" className="inputField" />
-                    <select name="continent">
-                        <option value="">Continent</option>
-                    </select>
-                </filter-wrapper>
-                <country-item>
-                    {countries.map((countriesItem, index) => (
-                        <section key={index} className="countriesItems">
+                {filteredCountriesByContinent.map((countriesItem, index) => (
+                    <section key={index} className="countriesItems">
+                        <div className="picWrapper" >
                             <img src={countriesItem.flags.svg} alt="" />
-                            <h2>{countriesItem.name.common}</h2>
-                            <p>{countriesItem.capital}</p>
-                        </section>
-                    ))}
-                </country-item>
+                        </div>
+                        <h2>Country: {countriesItem.name.common}</h2>
+                        <p> Capital: {countriesItem.capital}</p>
+                        <p>Continent: {countriesItem.region}</p>
+                    </section>
+                ))}
             </main>
         </>
     );
